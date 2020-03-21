b8ec82dc43cca3b62362a039b8c70d4b0d9ba318
Matthew Feng <Mattfeng2013@gmail.com>
2020-03-21 19:54:58 +1100

    init

---
+++ beer-catalog-service/src/test/java/com/example/beercatalogservice/BeerCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beercatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
