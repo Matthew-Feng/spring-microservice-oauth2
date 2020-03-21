c4a89104ffc77934c9c1ca725c766903cc1668d5
Matthew Feng <Mattfeng2013@gmail.com>
2020-03-21 20:02:08 +1100

    change to be only microservice

--- edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
@@ -1,27 +1,23 @@
 package com.example.edgeservice;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.ribbon.proxy.annotation.Hystrix;
-import feign.RequestInterceptor;
 import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;;import java.util.ArrayList;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-@EnableOAuth2Sso
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableDiscoveryClient
@@ -31,11 +27,6 @@ public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdgeServiceApplication.class, args);
-	}
-
-	@Bean
-	public RequestInterceptor getUserFeignClientInterceptor() {
-		return new UserFeignClientInterceptor();
 	}
 
 }
