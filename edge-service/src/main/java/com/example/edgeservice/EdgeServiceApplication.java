77f0e98f477f01511293599c07c3efb145e78a94
Matthew Feng <Mattfeng2013@gmail.com>
2020-03-22 11:22:24 +1100

    working condition

--- edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.CollectionModel;
@@ -21,7 +20,6 @@ import java.util.stream.Collectors;
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableDiscoveryClient
-@EnableZuulProxy
 @SpringBootApplication
 public class EdgeServiceApplication {
 
