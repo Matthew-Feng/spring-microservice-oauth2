e42fc6226474b7ddc34453a026ae64d16ed81989
Matthew Feng <Mattfeng2013@gmail.com>
2020-03-22 14:27:44 +1100

    add one more feign client

--- edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/example/edgeservice/EdgeServiceApplication.java
@@ -6,21 +6,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableZuulProxy
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
@@ -42,13 +48,32 @@ interface BeerClient {
 	CollectionModel<Beer> readBeers();
 }
 
+@FeignClient(value = "jplaceholder", url = "https://jsonplaceholder.typicode.com/", configuration = MyClientConfiguration.class)
+interface JSONPlaceHolderClient {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/posts")
+	List<Object> getPosts();
+
+	@RequestMapping(method = RequestMethod.GET, value = "/posts/{postId}", produces = "application/json")
+	Object getPostById(@PathVariable("postId") Long postId);
+}
+
 @RestController
 class GoodBeerApiAdapterRestController {
 
 	private final BeerClient beerClient;
+	private final JSONPlaceHolderClient jsonPlaceHolderClient;
+	
 
-	public GoodBeerApiAdapterRestController(BeerClient beerClient) {
+	public GoodBeerApiAdapterRestController(BeerClient beerClient,JSONPlaceHolderClient jsonPlaceHolderClient) {
 		this.beerClient = beerClient;
+		this.jsonPlaceHolderClient  = jsonPlaceHolderClient;
+	}
+
+	@GetMapping("/posts")
+	public List<Object> getPosts(){
+		return jsonPlaceHolderClient.getPosts();
+		
 	}
 
 	@HystrixCommand(fallbackMethod = "fallback")
